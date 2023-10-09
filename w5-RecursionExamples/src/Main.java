8d6dd131111ead417898624e53cd86824471b77e
Julian Kruithof <julian.kruithof@hva.nl>
2023-10-09 21:04:15 +0200

    add methods from exercise 0

--- w5-RecursionExamples/src/Main.java
+++ w5-RecursionExamples/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(factorial(4));
+        printFor("Hallo", 3);
+    }
+
+    static int factorial(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return n * factorial(n-1);
+    }
+
+    static void printFor(String message, int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(message);
+        printFor(message, n-1);
     }
 }
